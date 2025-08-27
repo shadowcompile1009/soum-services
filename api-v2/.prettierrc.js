800cb5e892e3ea602fa492d248f5c338c2e718f5
CodeSea0619 <timursarv1009@gmail.com>
2025-08-27 13:55:01 +0200

    init commit

---
+++ api-v2/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5', // trailing commas where valid in es5 (i.e. not function calls)
+};
